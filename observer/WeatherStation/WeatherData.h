57eff179ab9191a8bef63ce0cce7a5d83eefb4d4
Alex <ildarkin.alexey@gmail.com>
2016-03-17 18:26:54 +0300

    Upload observer (1, 3)

---
+++ observer/WeatherStation/WeatherData.h
@@ -0,0 +1,133 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include "Observer.h"
+#include <string>
+
+using namespace std;
+
+struct SWeatherInfo
+{
+	double temperature = 0;
+	double humidity = 0;
+	double pressure = 0;
+};
+
+class CDisplay: public IObserver<SWeatherInfo>
+{
+private:
+	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+		Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+		остается публичным
+	*/
+	void Update(SWeatherInfo const& data) override
+	{
+		std::cout << "Current Temp " << data.temperature << std::endl;
+		std::cout << "Current Hum " << data.humidity << std::endl;
+		std::cout << "Current Pressure " << data.pressure << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+};
+
+struct Stats
+{
+	double m_minStat = std::numeric_limits<double>::infinity();
+	double m_maxStat = -std::numeric_limits<double>::infinity();
+	double m_accStat = 0;
+	unsigned m_countAcc = 0;
+};
+
+class CStatsDisplay : public IObserver<SWeatherInfo>
+{
+private:
+	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+	Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+	остается публичным
+	*/
+	void SetStatsData(Stats &stat, double currentStat)
+	{
+		if (stat.m_minStat > currentStat)
+		{
+			stat.m_minStat = currentStat;
+		}
+		if (stat.m_maxStat < currentStat)
+		{
+			stat.m_maxStat = currentStat;
+		}
+		stat.m_accStat += currentStat;
+		++stat.m_countAcc;
+	}
+
+	void PrintStatsData(Stats &stat, std::string const &statName)
+	{
+		std::cout << "Max " << statName << ": " << stat.m_maxStat << std::endl;
+		std::cout << "Min " << statName << ": " << stat.m_minStat << std::endl;
+		std::cout << "Average " << statName << ": " << (stat.m_accStat / stat.m_countAcc) << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+
+	void Update(SWeatherInfo const& data) override
+	{
+		SetStatsData(m_tempStats, data.temperature);
+		PrintStatsData(m_tempStats, "Temp");
+
+		SetStatsData(m_humidityStats, data.humidity);
+		PrintStatsData(m_humidityStats, "Humidity");
+
+		SetStatsData(m_pressureStats, data.pressure);
+		PrintStatsData(m_pressureStats, "Pressure");
+	}
+private:
+	Stats m_tempStats;
+	Stats m_humidityStats;
+	Stats m_pressureStats;
+};
+
+class CWeatherData : public CObservable<SWeatherInfo>
+{
+public:
+	// Температура в градусах Цельсия
+	double GetTemperature()const
+	{
+		return m_temperature;
+	}
+	// Относительная влажность (0...100)
+	double GetHumidity()const
+	{
+		return m_humidity;
+	}
+	// Атмосферное давление (в мм.рт.ст)
+	double GetPressure()const
+	{
+		return m_pressure;
+	}
+
+	void MeasurementsChanged()
+	{
+		NotifyObservers();
+	}
+
+	void SetMeasurements(double temp, double humidity, double pressure)
+	{
+		m_humidity = humidity;
+		m_temperature = temp;
+		m_pressure = pressure;
+
+		MeasurementsChanged();
+	}
+protected:
+	SWeatherInfo GetChangedData()const override
+	{
+		SWeatherInfo info;
+		info.temperature = GetTemperature();
+		info.humidity = GetHumidity();
+		info.pressure = GetPressure();
+		return info;
+	}
+private:
+	double m_temperature = 0.0;
+	double m_humidity = 0.0;	
+	double m_pressure = 760.0;	
+};
---
+++ observer/WeatherStation/main.cpp
@@ -0,0 +1,22 @@
+#include "WeatherData.h"
+#include <memory>
+
+int main()
+{
+	CWeatherData wd;
+
+	shared_ptr<CDisplay> display = make_shared<CDisplay>();
+	wd.RegisterObserver(display.get(), 3);
+
+	shared_ptr<CStatsDisplay> statsDisplay = make_shared<CStatsDisplay>();
+	wd.RegisterObserver(statsDisplay.get(), 2);
+
+	wd.SetMeasurements(3, 0.7, 760);
+	wd.SetMeasurements(4, 0.8, 761);
+
+	wd.RemoveObserver(statsDisplay.get());
+
+	wd.SetMeasurements(10, 0.8, 761);
+	wd.SetMeasurements(-10, 0.8, 761);
+	return 0;
+}
